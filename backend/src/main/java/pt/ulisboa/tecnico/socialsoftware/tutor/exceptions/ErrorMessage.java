d19aee631f81114f70fbdbe122eeaba741884f21
henriquebg99 <henriquebg_99@hotmail.com>
2020-03-13 16:51:28 +0000

    little correction of ErrorMessage

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/exceptions/ErrorMessage.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/exceptions/ErrorMessage.java
@@ -4,14 +4,22 @@ public enum ErrorMessage {
     ACCESS_DENIED("You do not have permission to view this resource"),
     ANSWERS_IMPORT_ERROR("Error importing answers: %s"),
     ASSESSMENT_NOT_FOUND("Assessment not found with id %d"),
-    AUTHENTICATION_ERROR("Authentication Error"),
-    COURSE_EXECUTION_ACADEMIC_TERM_IS_EMPTY("The course execution academic term is empty"),
-    COURSE_EXECUTION_ACRONYM_IS_EMPTY("The course execution acronym is empty"),
-    COURSE_EXECUTION_MISMATCH("Course Execution %d does not have quiz %d"),
+    TOPIC_CONJUNCTION_NOT_FOUND("Topic Conjunction not found with id %d"),
     COURSE_EXECUTION_NOT_FOUND("Course execution not found with id %d"),
+
+    COURSE_NOT_FOUND("Course not found with name %s"),
     COURSE_NAME_IS_EMPTY("The course name is empty"),
-    COURSE_NOT_FOUND("Course not found with id %s"),
     COURSE_TYPE_NOT_DEFINED("The course type is not defined"),
+    COURSE_EXECUTION_ACRONYM_IS_EMPTY("The course execution acronym is empty"),
+    COURSE_EXECUTION_ACADEMIC_TERM_IS_EMPTY("The course execution academic term is empty"),
+    CANNOT_DELETE_COURSE_EXECUTION("The course execution cannot be deleted %s"),
+    USERNAME_NOT_FOUND("Username %s not found"),
+
+    QUIZ_USER_MISMATCH("Quiz %s is not assigned to student %s"),
+    QUIZ_MISMATCH("Quiz Answer Quiz %d does not match Quiz Question Quiz %d"),
+    QUESTION_OPTION_MISMATCH("Question %d does not have option %d"),
+    AUTHENTICATION_ERROR("Authentication Error"),
+    COURSE_EXECUTION_MISMATCH("Course Execution %d does not have quiz %d"),
 
     DUPLICATE_COURSE_EXECUTION("Duplicate course execution: %s"),
     DUPLICATE_TOPIC("Duplicate topic: %s"),
@@ -26,7 +34,13 @@ public enum ErrorMessage {
     QUESTION_CHANGE_CORRECT_OPTION_HAS_ANSWERS("Can not change correct option of answered question"),
 
     QUESTION_IS_USED_IN_QUIZ("Question is used in quiz %s"),
-    QUESTION_MISSING_DATA("Missing information for quiz"), // TODO check me
+    QUIZ_NOT_CONSISTENT("Field %s of quiz is not consistent"),
+    USER_NOT_ENROLLED("%s - Not enrolled in any available course"),
+    QUIZ_NO_LONGER_AVAILABLE("This quiz is no longer available"),
+    QUIZ_NOT_YET_AVAILABLE("This quiz is not yet available"),
+
+    NO_CORRECT_OPTION("Question does not have a correct option"),
+    QUESTION_MISSING_DATA("Missing information for quiz"),
     QUESTION_MULTIPLE_CORRECT_OPTIONS("Questions can only have 1 correct option"),
 
     QUESTION_NOT_FOUND("Question not found with id %d"),
@@ -35,23 +49,16 @@ public enum ErrorMessage {
     QUIZ_ANSWER_NOT_FOUND("Quiz answer not found with id %d"),
     QUIZ_HAS_ANSWERS("Quiz already has answers"),
 
-    QUIZ_MISMATCH("Quiz Answer Quiz %d does not match Quiz Question Quiz %d"),
-    QUIZ_NOT_CONSISTENT("Field %s of quiz is not consistent"),
     QUIZ_NOT_FOUND("Quiz not found with id %d"),
-    QUIZ_NOT_YET_AVAILABLE("This quiz is not yet available"),
-    QUIZ_NO_LONGER_AVAILABLE("This quiz is no longer available"),
 
+    CANNOT_OPEN_FILE("Cannot open file"),
     QUIZ_OPTION_MISMATCH("Quiz Question %d does not have option %d"),
     QUIZ_QUESTION_HAS_ANSWERS("Quiz question has answers"),
     QUIZ_QUESTION_NOT_FOUND("Quiz question not found with id %d"),
-    QUIZ_USER_MISMATCH("Quiz %s is not assigned to student %s"),
     TOPICS_IMPORT_ERROR("Error importing topics: %s"),
-    TOPIC_CONJUNCTION_NOT_FOUND("Topic Conjunction not found with id %d"),
     TOPIC_NOT_FOUND("Topic not found with id %d"),
     TOPIC_NOT_FOUND_NAME("Topic not found with name"),
-    USERNAME_NOT_FOUND("Username %s not found"),
     USERS_IMPORT_ERROR("Error importing users: %s"),
-    USER_NOT_ENROLLED("%s - Not enrolled in any available course"),
 
     USER_NOT_FOUND("User not found with id %d"),
     END_DATE_IS_EMPTY("End date is empty."),
