5ab8224e612911795100bccda454daf2a0538f6c
Dora Lourenço <dora.lourenco.dl@gmail.com>
2020-05-07 03:58:09 +0100

    Add ListClosedTournamentsPerformanceTest.groovy. Fix ListTournamentsPerformanceTest.groovy. Issue #87.

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/ListClosedTournamentsPerformanceTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/ListClosedTournamentsPerformanceTest.groovy
@@ -22,17 +22,18 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 @DataJpaTest
-class ListTournamentsPerformanceTest extends Specification{
+class ListClosedTournamentsPerformanceTest extends Specification{
     public static final String USER_USERNAME      = "username"
     public static final String USER_NAME          = "name"
     public static final int    USER_KEY           = 1
-    public static final int    BEGIN_DAYS         = 1
-    public static final int    END_DAYS           = 2
+    public static final int    BEGIN_MINUTES      = 1
+    public static final int    END_MINUTES        = 2
     public static final String TOPIC_NAME         = "topic"
     public static final String COURSE_NAME        = "course"
     public static final String ACRONYM            = "AS1"
     public static final String ACADEMIC_TERM      = "1 SEM"
     public static final int    NUMBER_QUESTIONS   = 5
+    public static final int    SLEEP              = 130000
 
     @Autowired
     TournamentRepository tournamentRepository
@@ -68,8 +69,8 @@ class ListTournamentsPerformanceTest extends Specification{
         userRepository.save(user)
 
         formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
-        beginDate = LocalDateTime.now().plusDays(BEGIN_DAYS)
-        endDate = LocalDateTime.now().plusDays(END_DAYS)
+        beginDate = LocalDateTime.now().plusMinutes(BEGIN_MINUTES)
+        endDate = LocalDateTime.now().plusMinutes(END_MINUTES)
 
         topicDto = new TopicDto()
         topicDto.setName(TOPIC_NAME)
@@ -95,14 +96,16 @@ class ListTournamentsPerformanceTest extends Specification{
             tournamentService.createTournament(USER_USERNAME, courseExecution.getId(), tournament)
         })
 
+        sleep(SLEEP);
+
     }
 
-    def 'performance testing to list 500 tournaments 10000 times'() {
+    def 'performance testing to list 500 closed tournaments 10000 times'() {
         given: '500 tournaments'
 
         when:
         1.upto(10000, {
-            tournamentService.listOpenTournaments(courseExecution.getId())
+            tournamentService.listClosedTournaments(courseExecution.getId())
         })
 
         then:
