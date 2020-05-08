732f0ae0295f66058e5fd72492bcd140a44c9521
marianabrejo <marianabrejo@gmail.com>
2020-05-08 11:43:59 +0100

    Issue #104 (tdp): fix cypress test error

--- frontend/tests/e2e/support/commands.js
+++ frontend/tests/e2e/support/commands.js
@@ -61,109 +61,106 @@ Cypress.Commands.add('setStatsPrivacy', () => {
 
 Cypress.Commands.add('checkTotalCreatedTournaments', () => {
   cy.contains(126);
+});
 
-  Cypress.Commands.add('goToClosedTournaments', () => {
-    cy.contains('Tournaments').click();
-    cy.contains('Closed').click();
-  });
+Cypress.Commands.add('goToClosedTournaments', () => {
+  cy.contains('Tournaments').click();
+  cy.contains('Closed').click();
+});
 
-  Cypress.Commands.add(
-    'createCourseExecution',
-    (name, acronym, academicTerm) => {
-      cy.get('[data-cy="createButton"]').click();
-      cy.get('[data-cy="Name"]').type(name);
-      cy.get('[data-cy="Acronym"]').type(acronym);
-      cy.get('[data-cy="AcademicTerm"]').type(academicTerm);
-      cy.get('[data-cy="saveButton"]').click();
-    }
-  );
+Cypress.Commands.add('createCourseExecution', (name, acronym, academicTerm) => {
+  cy.get('[data-cy="createButton"]').click();
+  cy.get('[data-cy="Name"]').type(name);
+  cy.get('[data-cy="Acronym"]').type(acronym);
+  cy.get('[data-cy="AcademicTerm"]').type(academicTerm);
+  cy.get('[data-cy="saveButton"]').click();
+});
 
-  Cypress.Commands.add('enrollTournaments', () => {
-    cy.get('ul li:first').click();
-    cy.get('ul i:first').should('have.class', 'fa-check-square');
-  });
+Cypress.Commands.add('enrollTournaments', () => {
+  cy.get('ul li:first').click();
+  cy.get('ul i:first').should('have.class', 'fa-check-square');
+});
 
-  Cypress.Commands.add('closeErrorMessage', (name, acronym, academicTerm) => {
-    cy.contains('Error')
-      .parent()
-      .find('button')
-      .click();
-  });
+Cypress.Commands.add('closeErrorMessage', (name, acronym, academicTerm) => {
+  cy.contains('Error')
+    .parent()
+    .find('button')
+    .click();
+});
 
-  Cypress.Commands.add('deleteCourseExecution', acronym => {
-    cy.contains(acronym)
+Cypress.Commands.add('deleteCourseExecution', acronym => {
+  cy.contains(acronym)
+    .parent()
+    .should('have.length', 1)
+    .children()
+    .should('have.length', 7)
+    .find('[data-cy="deleteCourse"]')
+    .click();
+});
+
+Cypress.Commands.add(
+  'createFromCourseExecution',
+  (name, acronym, academicTerm) => {
+    cy.contains(name)
       .parent()
       .should('have.length', 1)
       .children()
       .should('have.length', 7)
-      .find('[data-cy="deleteCourse"]')
+      .find('[data-cy="createFromCourse"]')
       .click();
-  });
+    cy.get('[data-cy="Acronym"]').type(acronym);
+    cy.get('[data-cy="AcademicTerm"]').type(academicTerm);
+    cy.get('[data-cy="saveButton"]').click();
+  }
+);
 
-  Cypress.Commands.add(
-    'createFromCourseExecution',
-    (name, acronym, academicTerm) => {
-      cy.contains(name)
-        .parent()
-        .should('have.length', 1)
-        .children()
-        .should('have.length', 7)
-        .find('[data-cy="createFromCourse"]')
-        .click();
-      cy.get('[data-cy="Acronym"]').type(acronym);
-      cy.get('[data-cy="AcademicTerm"]').type(academicTerm);
-      cy.get('[data-cy="saveButton"]').click();
-    }
-  );
+Cypress.Commands.add('createTournament', () => {
+  // select day 29
+  cy.get('#input-77').click();
+  cy.get(':nth-child(5) > :nth-child(6) > .v-btn > .v-btn__content').click();
+  cy.get('.v-time-picker-clock__item--active > span').click();
+  cy.get(
+    '.fade-transition-enter-active > .v-time-picker-clock > .v-time-picker-clock__inner > [style="left: 50%; top: 100%;"] > span'
+  ).click();
+  cy.get('.green--text > .v-btn__content').click();
 
-  Cypress.Commands.add('createTournament', () => {
-    // select day 29
-    cy.get('#input-77').click();
-    cy.get(':nth-child(5) > :nth-child(6) > .v-btn > .v-btn__content').click();
-    cy.get('.v-time-picker-clock__item--active > span').click();
-    cy.get(
-      '.fade-transition-enter-active > .v-time-picker-clock > .v-time-picker-clock__inner > [style="left: 50%; top: 100%;"] > span'
-    ).click();
-    cy.get('.green--text > .v-btn__content').click();
+  // select day 30
+  cy.get('#input-82').click();
+  cy.get(
+    '.v-dialog__content--active > .v-dialog > .v-sheet > .v-card__text > .v-tabs > .v-window > .v-window__container > .v-window-item > .v-picker > .v-picker__body > :nth-child(1) > .v-date-picker-table > table > tbody > :nth-child(5) > :nth-child(7) > .v-btn > .v-btn__content'
+  ).click();
+  cy.get(
+    '.v-window-item--active > .v-picker > .v-picker__body > .v-time-picker-clock__container > .v-time-picker-clock > .v-time-picker-clock__inner > .v-time-picker-clock__item--active > span'
+  ).click();
+  cy.get(
+    '.fade-transition-enter-active > .v-time-picker-clock > .v-time-picker-clock__inner > [style="left: 50%; top: 100%;"] > span'
+  ).click();
+  cy.get(
+    '.v-dialog__content--active > .v-dialog > .v-sheet > .v-card__actions > .green--text > .v-btn__content'
+  ).click();
 
-    // select day 30
-    cy.get('#input-82').click();
-    cy.get(
-      '.v-dialog__content--active > .v-dialog > .v-sheet > .v-card__text > .v-tabs > .v-window > .v-window__container > .v-window-item > .v-picker > .v-picker__body > :nth-child(1) > .v-date-picker-table > table > tbody > :nth-child(5) > :nth-child(7) > .v-btn > .v-btn__content'
-    ).click();
-    cy.get(
-      '.v-window-item--active > .v-picker > .v-picker__body > .v-time-picker-clock__container > .v-time-picker-clock > .v-time-picker-clock__inner > .v-time-picker-clock__item--active > span'
-    ).click();
-    cy.get(
-      '.fade-transition-enter-active > .v-time-picker-clock > .v-time-picker-clock__inner > [style="left: 50%; top: 100%;"] > span'
-    ).click();
-    cy.get(
-      '.v-dialog__content--active > .v-dialog > .v-sheet > .v-card__actions > .green--text > .v-btn__content'
-    ).click();
+  // 10 questions
+  cy.get('[data-cy="tenNumber"]').click();
 
-    // 10 questions
-    cy.get('[data-cy="tenNumber"]').click();
+  // select topic
+  cy.get(
+    ':nth-child(2) > .text-start > .v-data-table__checkbox > .v-icon'
+  ).click();
 
-    // select topic
-    cy.get(
-      ':nth-child(2) > .text-start > .v-data-table__checkbox > .v-icon'
-    ).click();
+  // create it
+  cy.get('[data-cy="createButton"]').click();
+});
 
-    // create it
-    cy.get('[data-cy="createButton"]').click();
-  });
+Cypress.Commands.add('createTournamentToClose', () => {
+  // select topic
+  cy.get(
+    ':nth-child(2) > .text-start > .v-data-table__checkbox > .v-icon'
+  ).click();
 
-  Cypress.Commands.add('createTournamentToClose', () => {
-    // select topic
-    cy.get(
-      ':nth-child(2) > .text-start > .v-data-table__checkbox > .v-icon'
-    ).click();
+  // create it
+  cy.get('[data-cy="createButton"]').click();
+});
 
-    // create it
-    cy.get('[data-cy="createButton"]').click();
-  });
-
-  Cypress.Commands.add('cancelTournament', () => {
-    cy.get(':nth-child(2) > .last-col > .fas').click();
-  });
+Cypress.Commands.add('cancelTournament', () => {
+  cy.get(':nth-child(2) > .last-col > .fas').click();
 });
